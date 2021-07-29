4ddf9a1405122d8fc19f3c035eec59deeb790841
Frederick Ouimet <fouimet@gmail.com>
2021-07-29 16:24:24 -0400

    Initial cauldron controller with feedbacks

--- Assets/CauldronController.cs
+++ Assets/CauldronController.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.Feedbacks;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CauldronController : MonoBehaviour
+public class CauldronController : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private ParticleSystem successParticles;
+    [SerializeField] private ParticleSystem failParticles;
+    [SerializeField] private MMFeedbacks createPotionFeedbacks;
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerClick(PointerEventData eventData)
     {
+        Debug.Log("Clicking the cauldron");
+        var particleFeedback = createPotionFeedbacks.GetComponent<MMFeedbackParticles>();
+        if (particleFeedback != null)
+        {
+            particleFeedback.BoundParticleSystem = failParticles;
+        }
         
+        createPotionFeedbacks.PlayFeedbacks();
     }
 }
