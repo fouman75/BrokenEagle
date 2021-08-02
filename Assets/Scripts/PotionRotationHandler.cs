6bee94f81cdb7708363dd344acee4fe2e5517130
Frederick Ouimet <fouimet@gmail.com>
2021-08-02 16:47:56 -0400

    change namespace.

--- Assets/Scripts/PotionRotationHandler.cs
+++ Assets/Scripts/PotionRotationHandler.cs
@@ -1,27 +1,30 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PotionRotationHandler : MonoBehaviour, IDragHandler
+namespace FOUOne.PotionCreator
 {
-    [SerializeField] private Transform potionContainer;
-    [SerializeField] private float maxRotationX = 45f;
-    [SerializeField] private float maxRotationZ = 45f;
-
-    private float _angleX;
-    private float _angleZ;
-
-    public void OnDrag(PointerEventData eventData)
+    public class PotionRotationHandler : MonoBehaviour, IDragHandler
     {
-        RotatePotion(eventData.delta);
-    }
+        [SerializeField] private Transform potionContainer;
+        [SerializeField] private float maxRotationX = 45f;
+        [SerializeField] private float maxRotationZ = 45f;
 
-    private void RotatePotion(Vector2 delta)
-    {
-        var drag = delta;
+        private float _angleX;
+        private float _angleZ;
 
-        _angleX = Mathf.Clamp(_angleX + drag.y, -maxRotationX, maxRotationX);
-        _angleZ = Mathf.Clamp(_angleZ - drag.x, -maxRotationZ, maxRotationZ);
+        public void OnDrag(PointerEventData eventData)
+        {
+            RotatePotion(eventData.delta);
+        }
+
+        private void RotatePotion(Vector2 delta)
+        {
+            var drag = delta;
+
+            _angleX = Mathf.Clamp(_angleX + drag.y, -maxRotationX, maxRotationX);
+            _angleZ = Mathf.Clamp(_angleZ - drag.x, -maxRotationZ, maxRotationZ);
         
-        potionContainer.rotation = Quaternion.Euler(_angleX, 0, _angleZ);
+            potionContainer.rotation = Quaternion.Euler(_angleX, 0, _angleZ);
+        }
     }
 }
--- Assets/Scripts/Wobble.cs
+++ Assets/Scripts/Wobble.cs
@@ -1,61 +1,64 @@
 using UnityEngine;
 
-public class Wobble : MonoBehaviour
+namespace FOUOne.PotionCreator
 {
-    [SerializeField] private float maxWobble = 0.03f;
-    [SerializeField] private float wobbleSpeed = 1f;
-    [SerializeField] private float recovery = 1f;
-    
-    private float _wobbleAmountX;
-    private float _wobbleAmountZ;
-    private float _wobbleAmountToAddX;
-    private float _wobbleAmountToAddZ;
-    private float _pulse;
-    private float _time = 0.5f;
-    
-    private Renderer _rend;
-    private Vector3 _lastPos;
-    private Vector3 _velocity;
-    private Vector3 _lastRot;
-    private Vector3 _angularVelocity;
-    
-    private static readonly int ShaderWobbleX = Shader.PropertyToID("_WobbleX");
-    private static readonly int ShaderWobbleZ = Shader.PropertyToID("_WobbleZ");
-
-    // Use this for initialization
-    void Start()
+    public class Wobble : MonoBehaviour
     {
-        _rend = GetComponent<Renderer>();
-    }
-    private void Update()
-    {
-        _time += Time.deltaTime;
-        // decrease wobble over time
-        _wobbleAmountToAddX = Mathf.Lerp(_wobbleAmountToAddX, 0, Time.deltaTime * (recovery));
-        _wobbleAmountToAddZ = Mathf.Lerp(_wobbleAmountToAddZ, 0, Time.deltaTime * (recovery));
+        [SerializeField] private float maxWobble = 0.03f;
+        [SerializeField] private float wobbleSpeed = 1f;
+        [SerializeField] private float recovery = 1f;
+    
+        private float _wobbleAmountX;
+        private float _wobbleAmountZ;
+        private float _wobbleAmountToAddX;
+        private float _wobbleAmountToAddZ;
+        private float _pulse;
+        private float _time = 0.5f;
+    
+        private Renderer _rend;
+        private Vector3 _lastPos;
+        private Vector3 _velocity;
+        private Vector3 _lastRot;
+        private Vector3 _angularVelocity;
+    
+        private static readonly int ShaderWobbleX = Shader.PropertyToID("_WobbleX");
+        private static readonly int ShaderWobbleZ = Shader.PropertyToID("_WobbleZ");
 
-        // make a sine wave of the decreasing wobble
-        _pulse = 2 * Mathf.PI * wobbleSpeed;
-        _wobbleAmountX = _wobbleAmountToAddX * Mathf.Sin(_pulse * _time);
-        _wobbleAmountZ = _wobbleAmountToAddZ * Mathf.Sin(_pulse * _time);
+        // Use this for initialization
+        void Start()
+        {
+            _rend = GetComponent<Renderer>();
+        }
+        private void Update()
+        {
+            _time += Time.deltaTime;
+            // decrease wobble over time
+            _wobbleAmountToAddX = Mathf.Lerp(_wobbleAmountToAddX, 0, Time.deltaTime * (recovery));
+            _wobbleAmountToAddZ = Mathf.Lerp(_wobbleAmountToAddZ, 0, Time.deltaTime * (recovery));
 
-        // send it to the shader
-        _rend.material.SetFloat(ShaderWobbleX, _wobbleAmountX);
-        _rend.material.SetFloat(ShaderWobbleZ, _wobbleAmountZ);
+            // make a sine wave of the decreasing wobble
+            _pulse = 2 * Mathf.PI * wobbleSpeed;
+            _wobbleAmountX = _wobbleAmountToAddX * Mathf.Sin(_pulse * _time);
+            _wobbleAmountZ = _wobbleAmountToAddZ * Mathf.Sin(_pulse * _time);
 
-        // velocity
-        var t = transform;
-        var pos = t.position;
-        var rot = t.rotation;
-        _velocity = (_lastPos - pos) / Time.deltaTime;
-        _angularVelocity = rot.eulerAngles - _lastRot;
+            // send it to the shader
+            _rend.material.SetFloat(ShaderWobbleX, _wobbleAmountX);
+            _rend.material.SetFloat(ShaderWobbleZ, _wobbleAmountZ);
 
-        // add clamped velocity to wobble
-        _wobbleAmountToAddX += Mathf.Clamp((_velocity.x + (_angularVelocity.z * 0.2f)) * maxWobble, -maxWobble, maxWobble);
-        _wobbleAmountToAddZ += Mathf.Clamp((_velocity.z + (_angularVelocity.x * 0.2f)) * maxWobble, -maxWobble, maxWobble);
+            // velocity
+            var t = transform;
+            var pos = t.position;
+            var rot = t.rotation;
+            _velocity = (_lastPos - pos) / Time.deltaTime;
+            _angularVelocity = rot.eulerAngles - _lastRot;
 
-        // keep last position
-        _lastPos = pos;
-        _lastRot = rot.eulerAngles;
+            // add clamped velocity to wobble
+            _wobbleAmountToAddX += Mathf.Clamp((_velocity.x + (_angularVelocity.z * 0.2f)) * maxWobble, -maxWobble, maxWobble);
+            _wobbleAmountToAddZ += Mathf.Clamp((_velocity.z + (_angularVelocity.x * 0.2f)) * maxWobble, -maxWobble, maxWobble);
+
+            // keep last position
+            _lastPos = pos;
+            _lastRot = rot.eulerAngles;
+        }
     }
 }
